1ed623d9cb2bf6321bcbe8aa09fd1c923600fe2c
orlov <s_m_orlov@mail.ru>
2025-08-27 23:22:29 +0300

    Interface good ver. 2

--- src/main/java/com/javarush/orlov/CaesarCipherApp.java
+++ src/main/java/com/javarush/orlov/CaesarCipherApp.java
@@ -6,52 +6,54 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 
-
 public class CaesarCipherApp {
-
 
     public static void main(String[] args) throws IOException {
 
-
-
-
         while (true) {
             int num = Validator.begin();
-            int key;
 
-            if (num == 0) {
-                System.out.println("Выход из программы. Пока!");
+            switch (num){
+                case  0:
+                    System.out.println("Выход из программы. Пока!");
+                    return;
+
+                case  1:
+                    encryptFile();
+                    System.out.println("Готово!");
+                    break;
+
+                case  2:
+                decryptFile();
+                System.out.println("Готово! ");
                 break;
 
-            } else if (num == 1) {
-
-
-                key = Validator.scanKey();
-
-                ArrayList<String> text = new ArrayList<>(Files.readAllLines(Path.of("text\\text.txt")));
-
-                FileService.createNewFile("text\\encrypted.txt");
-                Encrypt encrypt = new Encrypt();
-                ArrayList<String> result = new ArrayList<>(encrypt.encrypt(text, key));
-                FileService.writeFile("text\\encrypted.txt", result);
-                System.out.println("Готово!");
-
-            } else if (num == 2) {
-
-                key = Validator.scanKey();
-
-                ArrayList<String> text = new ArrayList<>(Files.readAllLines(Path.of("text\\encrypted.txt")));
-                FileService.createNewFile("text\\decrypt.txt");
-
-                Decrypt decrypt = new Decrypt();
-
-                FileService.writeFile("text\\decrypt.txt", decrypt.decrypt(text, key));
-                System.out.println("Готово! ");
-
+                default:
+                    System.out.println("Некорректный выбор, попробуйте снова.");
             }
-
             }
+        }
 
+        private static void encryptFile () throws IOException {
+            int key = Validator.scanKey();
+
+            ArrayList<String> text = new ArrayList<>(Files.readAllLines(Path.of("text\\text.txt")));
+
+            FileService.createNewFile("text\\encrypted.txt");
+            Encrypt encrypt = new Encrypt();
+            ArrayList<String> result = new ArrayList<>(encrypt.encrypt(text, key));
+            FileService.writeFile("text\\encrypted.txt", result);
+        }
+
+        private static void decryptFile () throws IOException {
+            int key = Validator.scanKey();
+
+            ArrayList<String> text = new ArrayList<>(Files.readAllLines(Path.of("text\\encrypted.txt")));
+            FileService.createNewFile("text\\decrypt.txt");
+
+            Decrypt decrypt = new Decrypt();
+
+            FileService.writeFile("text\\decrypt.txt", decrypt.decrypt(text, key));
         }
     }
 
